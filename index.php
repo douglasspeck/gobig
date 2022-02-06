82d0f581e94f48245852163c69f1fea547e22a84
douglasspeck <speck.empresarial@gmail.com>
2022-02-06 11:48:46 -0300

    Started the New Project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <?php
+        $title = 'Projeto Go Big – Home';
+        $keywords = '';
+        $css = ['main', 'header', 'fonts', 'home'];
+        $js = [];
+        include 'PHP/head.php';
+    ?>
+<body>
+    <?php include 'PHP/header.php' ?>
+    
+</body>
+</html>
