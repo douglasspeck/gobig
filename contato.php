8c5b7defed840414b14a105cd2beb7cbfd01b625
douglasspeck <speck.empresarial@gmail.com>
2022-02-13 20:59:00 -0300

    Contact Page (not finished)

--- contato.php
+++ contato.php
@@ -3,12 +3,30 @@
     <?php
         $title = 'Informação de Contato – Projeto Go Big';
         $keywords = 'Educação';
-        $css = ['main', 'header', 'fonts', 'footer'];
+        $css = ['main', 'header', 'fonts', 'contato', 'footer'];
         $resources = [];
         include 'PHP/head.php';
     ?>
 <body hidden>
     <?php include 'PHP/header.php'; ?>
+    <main>
+        <div id="contact-wrapper">
+            <div>
+                <div class="mail-icon">
+                    
+                </div>
+            </div>
+            <div>
+                <h1>Escreva-nos!</h1>
+                <form action="">
+                    <input type="text" name="nome" placeholder="Seu Nome da Silva">
+                    <input type="text" name="email" placeholder="exemplo@seuemail.com">
+                    <textarea name="message" id="" cols="30" placeholder="Escreva sua mensagem aqui."></textarea>
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
+        </div>
+    </main>
     <?php
         include 'PHP/footer.php';
         
