e282e28c374bb5be4763930a6e8d35a91d26490f
douglasspeck <speck.empresarial@gmail.com>
2022-02-17 11:57:44 -0300

    Added Mobile Header

--- PHP/header.php
+++ PHP/header.php
@@ -1,10 +1,41 @@
 <?php
 
+    $pages = [
+        ['Home', '/', 'Página Inicial'],
+        ['O Projeto', 'o-projeto', 'Informações sobre o Projeto'],
+        ['Quero Participar', 'contato', 'Entre em Contato']
+    ];
+
     $active_page = str_replace('.php', '', $_SERVER['REQUEST_URI']);
     $active_page = str_replace('/', '', $active_page);
 
     echo '
     <input type="checkbox" id="dark-mode" onclick="darkMode();" checked hidden>
+    <input type="checkbox" id="toggle-menu" hidden>
+    <label for="toggle-menu">
+        <nav>';
+    
+        for ($i = 0; $i < count($pages); $i++) {
+            
+            echo '<div class="menu';
+
+            if ($pages[$i][1] == $active_page || $pages[$i][1] == '/' && $active_page == '') { echo ' active'; }
+            
+            echo '"><a href="' . $pages[$i][1];
+
+            if ($isLocal == true && $pages[$i][1] !== '/' && $pages[$i][1] !== '') {
+                echo '.php';
+            }
+
+            echo '"';
+
+            if ($pages[$i][2] !== '') { echo ' title="' . $pages[$i][2] . '"'; }
+
+            echo '>' . $pages[$i][0] . '</a></div>';
+
+        };
+    
+    echo '</nav></label>
     <header>
         <a href="/" title="Página Inicial">';
     
@@ -17,12 +48,6 @@
 
     echo '</a>
         <nav id="navbar">';
-
-    $pages = [
-        ['Home', '/', 'Página Inicial'],
-        ['O Projeto', 'o-projeto', 'Informações sobre o Projeto'],
-        ['Quero Participar', 'contato', 'Entre em Contato']
-    ];
 
     for ($i = 0; $i < count($pages); $i++) {
         
